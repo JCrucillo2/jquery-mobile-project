a9354ae4e4472b25669a7bed642494f210bd672b
Junnel Crucillo <71023403+JCrucillo2@users.noreply.github.com>
2022-03-13 22:58:09 -0600

    changed geocoding to geolocation api

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-let geocoder = new google.maps.Geocoder();
-
-if (geocoder) {
-    geocoder.geocode(
-        {
-            address: "810 Horsehill Rd NW, Edmonton, Alberta",
-        },
-        function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                let lat = results[0].geometry.location.lat();
-                let lng = results[0].geometry.location.lng();
-
-                console.log(lng);
-                let mapOptions = {
-                    center: new google.maps.LatLng(lat, lng),
-                    zoom: 16,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    draggable: false,
-                    zoomControl: false,
-                    scrollwheel: false,
-                    disableDoubleClickZoom: true,
-                    mapTypeControl: false,
-                    fullscreenControl: false,
-                    streetViewControl: false,
-                };
-
-                let map = new google.maps.Map(
-                    document.getElementById("map-canvas"),
-                    mapOptions
-                );
-
-                let marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(lat, lng),
-                    map: map,
-                    title: "CharacterDB Site",
-                });
-            }
-        }
+var defaultLatLng = new google.maps.LatLng(
+    53.589188757639185,
+    -113.33148788990094
+);
+if (navigator.geolocation) {
+    function success(pos) {
+        drawMap(
+            new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude)
+        );
+    }
+    function fail(error) {
+        drawMap(defaultLatLng);
+    }
+    navigator.geolocation.getCurrentPosition(success, fail, {
+        maximumAge: 500000,
+        enableHighAccuracy: true,
+        timeout: 6000,
+    });
+} else {
+    drawMap(defaultLatLng);
+}
+function drawMap(latlng) {
+    var myOptions = {
+        zoom: 10,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false,
+    };
+    var map = new google.maps.Map(
+        document.getElementById("map-canvas"),
+        myOptions
     );
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Greetings!",
+    });
 }
